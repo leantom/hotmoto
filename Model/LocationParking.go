61c8160bf1df80e9219bd21280fa624c8243e715
leantom <xuanquang48@gmail.com>
2018-03-02 17:15:31 +0700

    Add files via upload

---
+++ Model/LocationParking.go
@@ -0,0 +1,18 @@
+package Model
+
+import "gopkg.in/mgo.v2/bson"
+
+// Represents a movie, we uses bson keyword to tell the mgo driver how to name
+// the properties in mongodb document
+
+type location struct {
+	Coordinates []int `bson:"coordinates" json:"coordinates"`
+	Type_Location	string `bson:"type" json:"type"`
+}
+
+type LocationParking struct {
+	ID          bson.ObjectId `bson:"_id" json:"id"`
+	Location     location  `bson:"location" json:"location"`
+	Name  		string        `bson:"name" json:"name"`
+}
+
