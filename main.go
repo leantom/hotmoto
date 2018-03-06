8bcb01cd8748853be6294a977daab2c254523ec0
Lê An <xuanquang48@gmail.com>
2018-03-06 11:32:17 +0700

    update destintion

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
-	"log"
-	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+
+	"encoding/json"
+
+
 )
 
+var locationParkingDAO = LocationParkingDAO{}
+
+
+// Fetch Example
+
+func LocationFisrtParking(w http.ResponseWriter, r *http.Request) {
+
+	res, err := locationParkingDAO.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, res)
+}
+
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+
+func init() {
+	Connect()
+}
 
 func main() {
-	fmt.Println("Sleep a little...")
-	log.Print("Hello world")
+
+	r := mux.NewRouter()
+	//LocationFisrtParking
+	r.HandleFunc("/parkings", LocationFisrtParking).Methods("GET")
+
 }
