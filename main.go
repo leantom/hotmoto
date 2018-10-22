f7826f2a231aed71999b792dad67c2983c7123a7
Lê An <xuanquang48@gmail.com>
2018-10-22 11:27:06 +0700

    update url device token

--- main.go
+++ main.go
@@ -141,10 +141,10 @@ func FindAllUser(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	users, err := Module.FindAll()
-
+	println("user finding all")
 	if err != nil {
 
-		respondWithError(w, http.StatusBadRequest, err.Error())
+		respondWithError(w, http.StatusNotFound, err.Error())
 		return
 	}
 	respondWithJson(w, http.StatusCreated, users)
@@ -331,33 +331,33 @@ func prog(state overseer.State) {
 
 
 
-	r.HandleFunc("api/home", LocationFisrtParking).Methods("GET")
+	r.HandleFunc("/api/home", LocationFisrtParking).Methods("GET")
 
-	r.HandleFunc("api/users", FindAllUser).Methods("GET")
+	r.HandleFunc("/api/users", FindAllUser).Methods("GET")
 
-	r.HandleFunc("api/users/login", Login).Methods("POST")
+	r.HandleFunc("/api/users/login", Login).Methods("POST")
 
-	r.HandleFunc("api/users/register", InsertUser).Methods("POST")
+	r.HandleFunc("/api/users/register", InsertUser).Methods("POST")
 
-	r.HandleFunc("api/users", UpdateUser).Methods("PUT")
+	r.HandleFunc("/api/users", UpdateUser).Methods("PUT")
 
-	r.HandleFunc("api/parkings", InsertParking).Methods("POST")
+	r.HandleFunc("/api/parkings", InsertParking).Methods("POST")
 
-	r.HandleFunc("api/parkings/updateCost", UpdatePriceParking).Methods("POST")
+	r.HandleFunc("/api/parkings/updateCost", UpdatePriceParking).Methods("POST")
 
-	r.HandleFunc("api/parkings/updateSlot", UpdateSlotParking).Methods("POST")
+	r.HandleFunc("/api/parkings/updateSlot", UpdateSlotParking).Methods("POST")
 
-	r.HandleFunc("api/parkings", UpdateParking).Methods("PUT")
+	r.HandleFunc("/api/parkings", UpdateParking).Methods("PUT")
 
-	r.HandleFunc("api/parkings", deleteParking).Methods("DELETE")
+	r.HandleFunc("/api/parkings", deleteParking).Methods("DELETE")
 
-	r.HandleFunc("api/parkings", LocationFisrtParking).Methods("GET")
+	r.HandleFunc("/api/parkings", LocationFisrtParking).Methods("GET")
 
-	r.HandleFunc("api/parkings/users", FindParksByUser).Methods("POST")
+	r.HandleFunc("/api/parkings/users", FindParksByUser).Methods("POST")
 
-	r.HandleFunc("api/parkings/getNearCurrents", FindingParkingWithCurrentLocation).Methods("POST")
+	r.HandleFunc("/api/parkings/getNearCurrents", FindingParkingWithCurrentLocation).Methods("POST")
 
-	r.HandleFunc("api/uploads", Module.UploadFiles).Methods("POST")
+	r.HandleFunc("/api/uploads", Module.UploadFiles).Methods("POST")
 
 	// push notification
 	r.HandleFunc("/api/pushNotificationSingle",Module.PushNotificationSingle).Methods("POST")
