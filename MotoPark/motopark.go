5a9ca4ef21ca55e459ea75e0c4d75596a0603d77
Le an <xuanquang48@gmail.com>
2019-01-18 14:15:51 +0700

    fix update totalSlot to total

--- MotoPark/motopark.go
+++ MotoPark/motopark.go
@@ -4,60 +4,60 @@ import (
 	"fmt"
 	"log"
 
-	"gopkg.in/mgo.v2"
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type MotoPark struct {
 	ID       bson.ObjectId `bson:"_id" json:"id"`
 	Location struct {
-		Type        string `json:"type"`
-		Coordinates []float64  `json:"coordinates"`
+		Type        string    `json:"type"`
+		Coordinates []float64 `json:"coordinates"`
 	} `json:"location"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	Phone       string `json:"phone"`
-	Total       int    `json:"total"`
-	AvailableSlot int `json:"AvailableSlot"`
-	OpenTime     int `json:"openTime"`
-	CloseTime     int `json:"closeTime"`
-	Status int `json:"status"`
-	Fullname     string `json:"fullname"`
-	Cost int `json:"cost"`
-	NumberHours int `json:"numberHours"`
-	Username     string `json:"username"`
-	UserID     string `json:"userID"`
-	Email string `json:"email"`
-	ImageURL string `json:"imageUrl"`
+	Name            string `json:"name"`
+	Address         string `json:"address"`
+	Phone           string `json:"phone"`
+	Total           int    `json:"total"`
+	AvailableSlot   int    `json:"AvailableSlot"`
+	OpenTime        int    `json:"openTime"`
+	CloseTime       int    `json:"closeTime"`
+	Status          int    `json:"status"`
+	Fullname        string `json:"fullname"`
+	Cost            int    `json:"cost"`
+	NumberHours     int    `json:"numberHours"`
+	Username        string `json:"username"`
+	UserID          string `json:"userID"`
+	Email           string `json:"email"`
+	ImageURL        string `json:"imageUrl"`
 	DescriptionPark string `json:"description_park"`
-	Type int `json:"type"`
+	Type            int    `json:"type"`
 }
 
 type PriceParkRequest struct {
-	Cost int `json:"cost"`
+	Cost   int    `json:"cost"`
 	IdPark string `json:"id_park"`
 }
 
 type SlotParkRequest struct {
-	Slot int `json:"slot"`
+	Slot   int    `json:"slot"`
 	IdPark string `json:"id_park"`
 }
 
 type SlotPark struct {
-	ID       bson.ObjectId `bson:"_id" json:"id"`
-	AvailableSlot int `json:"available_slot"`
-	ParkID string `json:"park_id"`
-	MaximumSlot int `json:"maximum_slot"`
+	ID            bson.ObjectId `bson:"_id" json:"id"`
+	AvailableSlot int           `json:"available_slot"`
+	ParkID        string        `json:"park_id"`
+	MaximumSlot   int           `json:"maximum_slot"`
 }
 
 type PositionParking struct {
-	Type     string        `json:"type"`
-	coordinates [2]float64    `json:"coordinates"`
+	Type        string     `json:"type"`
+	coordinates [2]float64 `json:"coordinates"`
 }
 
 type FindingNearLocation struct {
-	Position [2]float64    `bson:"position" json:"position"`
-	Scope int `bson:"scope" json:"scope"`
+	Position [2]float64 `bson:"position" json:"position"`
+	Scope    int        `bson:"scope" json:"scope"`
 }
 
 const (
@@ -86,7 +86,7 @@ func checkDBLostConnect() {
 		fmt.Println("Mongo server connected")
 		db = session.DB(DB)
 	}
-	
+
 }
 
 func FindAll() ([]MotoPark, error) {
@@ -109,7 +109,7 @@ func FindById(userID string) (MotoPark, error) {
 
 func FindParksByUserID(userID string) ([]MotoPark, error) {
 	var parks []MotoPark
-	err := db.C(COLLECTION).Find(bson.M{"username":userID}).All(&parks)
+	err := db.C(COLLECTION).Find(bson.M{"username": userID}).All(&parks)
 	return parks, err
 }
 
@@ -124,14 +124,14 @@ func Update(park MotoPark) error {
 	return err
 }
 
-func UpdateCost(parkId string,cost int) (MotoPark, error) {
+func UpdateCost(parkId string, cost int) (MotoPark, error) {
 	var park MotoPark
 	err := db.C(COLLECTION).Find(bson.M{"_id": bson.ObjectIdHex(parkId)}).One(&park)
 	park.Cost = cost
-	return  park,err
+	return park, err
 }
 
-func UpdateAvailableSlot(parkId string,slot int)  (MotoPark, error) {
+func UpdateAvailableSlot(parkId string, slot int) (MotoPark, error) {
 	var park MotoPark
 	err := db.C(COLLECTION).Find(bson.M{"_id": bson.ObjectIdHex(parkId)}).One(&park)
 	if slot >= park.Total {
@@ -139,9 +139,8 @@ func UpdateAvailableSlot(parkId string,slot int)  (MotoPark, error) {
 	} else {
 		park.AvailableSlot = park.Total - slot
 	}
-	return  park,err
+	return park, err
 }
-
 
 func Delete(parkingId string) error {
 
@@ -154,7 +153,7 @@ func FindNearLocationParking(findingLocation FindingNearLocation) ([]MotoPark, e
 
 	collection := db.C(COLLECTION)
 	long := findingLocation.Position[0]
-	lat :=  findingLocation.Position[1]
+	lat := findingLocation.Position[1]
 
 	scope := findingLocation.Scope
 	log.Print(findingLocation.Position)
@@ -163,8 +162,7 @@ func FindNearLocationParking(findingLocation FindingNearLocation) ([]MotoPark, e
 			"$nearSphere": bson.M{
 				"$geometry": bson.M{
 					"type":        "Point",
-					"coordinates":  []float64{long, lat},
-
+					"coordinates": []float64{long, lat},
 				},
 				"$maxDistance": scope,
 			},
